a50594fa3f2a8f6c284079c755afaac252b49e7b
ilsoon156 <ilsoon156@naver.com>
2019-07-16 16:18:11 +0900

    init

---
+++ problem3.js
@@ -0,0 +1,25 @@
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+const { PrivateKey, PublicKey, Signature, Aes, key_utils, config } = require('eosjs-ecc');
+const { TextEncoder, TextDecoder } = require('util');
+const fetch = require('node-fetch');
+
+let privateKeys = [];// user private keys
+const rpc = new JsonRpc('http://192.168.1.75:8010', { fetch });
+const signatureProvider = new JsSignatureProvider(privateKeys);
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+async function main() {
+    try {
+        const result = await rpc.get_block(300000);
+        const result2 = await rpc.get_account(result.producer);
+        const result3 = await rpc.get_currency_balance('eosio.token', result2.account_name, 'EOS');
+        console.log(result3);
+    } catch (error) {
+        console.error(error);
+        
+    }
+
+}
+
+main();
